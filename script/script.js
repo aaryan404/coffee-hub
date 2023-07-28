022a1bc7c1beb438b9c01bde097e9874242c5165
aaryan404 <aryankhatri655@gmail.com>
2023-07-28 15:54:32 -0400

    made a new project similar as the previous version but added the phone breakpoint and made some major changes in the pages.

---
+++ script/script.js
@@ -0,0 +1,14 @@
+
+function burger() {
+    document.getElementById("cburger").style.visibility = "hidden";
+    document.getElementById("crossicon").style.visibility = "visible";
+    document.getElementById("nav").style.visibility = "visible";
+    document.getElementById("nav").style.animation = "simpleanimation forwards 1s ease-in-out;";
+    // add keyframes using javascript
+
+}
+function cross() {
+    document.getElementById("nav").style.visibility = "hidden";
+    document.getElementById("cburger").style.visibility = "visible";
+    document.getElementById("crossicon").style.visibility = "hidden";
+}
